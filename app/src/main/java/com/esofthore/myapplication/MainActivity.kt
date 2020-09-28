f1a9a80ed3e3c73cd78cf016292d6e53b4c968bf
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-09-28 00:29:44 -0300

    Initial project setup

---
+++ app/src/main/java/com/esofthore/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.esofthore.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
